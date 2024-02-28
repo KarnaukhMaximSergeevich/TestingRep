578461cd38fe255398a48c3e242109ddc7f25b65
Max <Maximaximus18@gmail.com>
2024-02-28 11:00:51 +0200

    Simple_run_app

---
+++ open_app/Test_open_app#1.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+path = r"D:\test.txt"
+path2 = r"'C:\Users\Maxim\AppData\Local\osu!'"
+
+#subprocess.run(['start', '', path], shell=True)
+
+os.system('"C://Users//Maxim//AppData//Local//osu!//osu!.exe"')
